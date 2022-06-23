138aad3c300a285b6d4e43140fcf39219a1ed896
jonghyunyang <jonghyun5646@naver.com>
2022-06-23 12:04:16 +0900

    Django3 view, template 수정 - 클라이언트측/서버측 유효성 검사, Member 객체 생성 후 DB 저장, 가입 완료시 ok 페이지로 전환

--- member/models.py
+++ member/models.py
@@ -5,6 +5,8 @@ from django.db import models
 # Create your models here.
 
 
+# Django 에서 폼 데이터를 다루는 방법은 크게 2가지 - Model, ModelForm
+# member 테이블의 구조와 유사하게 member 모델 정의 - Model
 class Member(models.Model):
     id = models.AutoField(primary_key=True)
     userid = models.CharField(max_length=18, unique=True)
