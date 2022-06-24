24d3e69db8e7736e2211af3663289e1fe7cfb453
jonghyunyang <jonghyun5646@naver.com>
2022-06-24 15:20:27 +0900

    Django3 board 새글쓰기, 리스트 출력

--- board/views.py
+++ board/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+import board
+from django.shortcuts import render, redirect
+from board.models import Board
+from member.models import Member
 
 # Create your views here.
 
 
 def list(request):
-    return render(request, 'board/list.html')
+    bdlist = board.objects.values('id','title','userid','regdate','views').order_by('-id')
+
+    context = {'bds': bdlist}
+    return render(request, 'board/list.html', context)
 
 
 def view(request):
@@ -12,4 +18,26 @@ def view(request):
 
 
 def write(request):
-    return render(request, 'board/write.html')
+    returnPage = 'board/write.html'
+    form = ''
+    error = ''
+
+    if request.POST == 'GET':
+        return render(request, returnPage)
+
+    elif request.POST == 'POST':
+        form = request.POST.dict()
+
+        # 유효성 검사
+        if not (form['title'] and form['contents']):
+            error = '제목이나 본문을 작성하세요!'
+        else:
+            # 입력한 게시글을 Board 객체에 담음
+            board = Board(title=form['title'], contents=form['contents'], userid=Member.objects.get(pk=form['memberid']))
+            board.save()    # Board 객체에 담은 게시글을 테이블에 저장
+
+            return redirect('/list')
+
+        context = {'form': form, 'error': error}
+
+        return render(request, returnPage, context)
--- member/views.py
+++ member/views.py
@@ -1,16 +1,13 @@
-from django.contrib.auth.hashers import make_password
-from django.contrib.auth.handlers.modwsgi import check_password
+from django.contrib.auth.hashers import make_password, check_password
 from django.shortcuts import render, redirect
 
 # Create your views here.
-
-
-# 회원가입 처리 함수 : 127.0.0.1:8000/join/
+# 회원가입 처리 함수 : 127.0.0.1/join
 from member.models import Member
 
 
 def join(request):
-    returnPage= 'member/join.html'
+    returnPage = 'member/join.html'
 
     if request.method == 'GET':
         return render(request, returnPage)
@@ -22,27 +19,27 @@ def join(request):
 
         # 유효성 검사 1/2, 3/4/5
         error = ''  # 검사 결과 저장용 변수
-        if not (form['userid'] and form['passwd'] and
-                form['repasswd'] and form['name'] and form['email']):
-            error = '<li>입력값이 누락되었습니다!</li>'
+        if not (form['userid'] and form['passwd'] and form['repasswd'] and form['name'] and form['email']):
+            error = '입력값이 누락되었습니다!'
         elif form['passwd'] != form['repasswd']:
-            error = '<li>비밀번호가 일치하지 않습니다!</li>'
+            error = '비밀번호가 일치하지 않습니다!'
         else:
-            # 입력한 회원정보를 Member 객체에 담음
+            # 입력한 회원정보를 Member객체에 담음
             member = Member(
                 userid=form['userid'],
-                passwd=make_password(form['passwd']),   # 비밀번호는 암호화
-                name=form['name'],
-                email=form['email']
+                passwd=make_password(form['passwd']),  # 비밀번호는 암호화
+                name=form['name'], email=form['email']
             )
-            # Member 객체에 담은 회원정보를 member 테이블에 저장
+            # Member객체에 담은 회원정보를 member테이블에 저장
             member.save()
 
-            # 회원가입 성공시 joinok.html을 띄움
+            # 회원가입 성공시 joinok.html를 띄움
             returnPage = 'member/joinok.html'
 
-        # 유효성 검사를 실패하는 경우 오류내용을 join.html에 표시하기 위해 dict 변수에 저장
-        # 또한, 이미 입력했던 회원정보를 다시 join.html에 표시하기 위해 form이라는 dict 변수 생성
+        # 유효성 검사를 실패하는 경우
+        # 오류내용을 join.html에 표시하기 위해 dict 변수에 저장
+        # 또한, 이미 입력했던 회원정보를 다시 join.html에 표시하기 위해
+        # form이라는 dict 변수 생성
         context = {'form': form, 'error': error}
 
         return render(request, returnPage, context)
@@ -51,7 +48,7 @@ def join(request):
 def login(request):
     returnPage = 'member/login.html'
 
-    if request.method == 'GET':
+    if request.method == "GET":
         return render(request, returnPage)
 
     elif request.method == 'POST':
@@ -65,28 +62,35 @@ def login(request):
             # 입력한 아이디로 회원정보가 테이블에 있는지 여부 확인
             try:
                 member = Member.objects.get(userid=form['userid'])
-            except:
-                pass
+            except Member.DoesNotExist:
+                member = None
 
             if member and check_password(form['passwd'], member.passwd):
-
+                # 아이디와 비밀번호 인증을 정상적으로 마쳤다면
+                # 세션변수에 인증정보를 저장해 둠
                 request.session['userid'] = form['userid']
-                return redirect('/')    # index 페이지로 이동
+
+                # 로그인한 사용자의 id도 조회해서 세션변수에 저장해 둠
+                id = Member.objects.all().filter(userid=form['userid']).values_list('id')[0][0]
+                request.session['userid_id'] = id
+                print(id)
+
+                return redirect('/')  # index 페이지로 이동
             else:
                 error = '아이디나 비밀번호가 틀립니다!'
 
         context = {'error': error}
-
         return render(request, returnPage, context)
 
 
 def myinfo(request):
-    member = {}
+    return render(request, 'member/myinfo.html')
 
+
+def logout(request):
+    # 만약 세션변수 userid가 존재하면 세션변수 삭제
     if request.session.get('userid'):
-        userid = request.session.get('userid')
+        del (request.session['userid'])
 
-        member = Member.objects.get(userid=userid)
-
-    context = {'member': member}
-    return render(request, 'member/myinfo.html', context)
+    # 로그아웃 후 index로 이동
+    return redirect('/')
